a40a1e2022a932f4fe24fd18ac223d87e14eeadf
Xyr3760 <167469422+Xyr3760@users.noreply.github.com>
2024-06-04 03:06:28 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center">
+<img src="https://readme-typing-svg.herokuapp.com?color=%2336BCF7&center=true&vCenter=true&lines=S+C+R+I+P+T+ㅤBYㅤ+XYRVPN+S+T+O+R+E" />
+</p>
+
+        ### SUPPORT OS LINUX
+<p align="center"><img src="https://img.shields.io/static/v1?style=for-the-badge&logo=debian&label=Debian%2010&message=Buster&color=purple">  <img src="https://img.shields.io/static/v1?style=for-the-badge&logo=ubuntu&label=Ubuntu%2020&message=Lts&color=red">
+</p>
+
+
+### ORDER/SEWA SCRIPT TUNNELING
+CONTACT:
+Telegram: t.me/@STORE_HANZZ
+Wa: wa.me/+6287872750080
+
+### INSTALL SCRIPT 
+```
+apt install -y && apt update -y && apt upgrade -y && wget -q https://raw.githubusercontent.com/xyoruz/scriptvpn/main/premi.sh && chmod +x premi.sh && ./premi.sh
+```
+
+## UPDATE SCRIPT
+```
+wget -q https://raw.githubusercontent.com/xyoruz/scriptvpn/main/update.sh && chmod +x update.sh && ./update.sh
+```
+
+### SETTING CLOUDFLARE
+```
+- SSL/TLS : FULL
+- SSL/TLS Recommender : OFF
+- GRPC : ON
+- WEBSOCKET : ON
+- Always Use HTTPS : OFF
+- UNDER ATTACK MODE : OFF
+```
+### INFO PORT
+```
+- PORT WEBSOCKET » 80
+- PORT TLS / SSL » 443
+- PORT HANCED WS » 80 » 8080
+- PORT NOOBZVPN  » 2082 » 8880  
+```
+### `WARNING !`
+```
+Jika Mendapatkan Status Service Off
+Silahkan Restart Service.
+Jika Statsus Service Masih Off
+Silahkan Reboot vps kalian
+```
---
+++ update.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+clear
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+res1() {
+    wget https://raw.githubusercontent.com/Xyr3760/autoscript-vps-gratis/main/menu/menu.zip
+    unzip menu.zip
+    chmod +x menu/*
+    mv menu/* /usr/local/sbin
+    rm -rf menu.zip
+    rm -rf update.sh
+    wget -qO- fv-tunnel "https://raw.githubusercontent.com/Xyr3760/autoscript-vps-gratis/main/config/fv-tunnel" 
+    chmod +x fv-tunnel 
+    bash fv-tunnel
+    rm -rf fv-tunnel
+}
+function noobzvpn() {
+wget "${REPO}/noobzvpns.zip"
+unzip noobzvpns.zip
+bash install.sh
+rm noobzvpns.zip
+systemctl restart noobzvpns
+print_success "NOOBZVPN"
+}
+netfilter-persistent
+clear
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e " \e[1;97;101m UPDATE SCRIPT SEDANG BERJALAN !             \e[0m"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e ""
+echo -e "  \033[1;91m Update Script Service\033[1;37m"
+fun_bar 'res1'
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
+echo -e ""
+read -n 1 -s -r -p "Press [ Enter ] to back on menu"
+menu
